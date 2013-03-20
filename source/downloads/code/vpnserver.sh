2c31f9826d85ce4227764b2a46d8848d65f7aa3f
Lincoln Lee <git@lincoln.hk>
2013-03-21 02:41:34 +0800

    4th post

--- source/downloads/code/vpnserver.sh
+++ source/downloads/code/vpnserver.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-# chkconfig: 2345 99 01
-# description: SoftEther VPN Server
+### BEGIN INIT INFO
+# Provides:          vpnserver
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable Softether by daemon.
+### END INIT INFO
 DAEMON=/usr/local/vpnserver/vpnserver
 LOCK=/var/lock/subsys/vpnserver
 test -x $DAEMON || exit 0
